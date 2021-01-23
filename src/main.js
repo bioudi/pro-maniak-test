7a58ed1d4d534306da10a91f0eefb143f79cecea
Amine Bioudi <bioudiamine@gmail.com>
2021-01-23 21:04:00 +0100

    Cleaning

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueAxios from "vue-axios";
-// Import axios
 import axios from "./axios";
 
 Vue.use(VueAxios, axios);
